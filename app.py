4823966562ac120b51f66c0b7cc4867bc9aec192
Sumit Bindra <sumitbindra@gmail.com>
2023-12-05 00:49:07 -0800

    first commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     if 'audio' not in request.files:
